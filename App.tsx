20e63bac7faf75eb6c3c5f6ce3d366f5dda47307
Lucas Vieira <lucas_99silva@hotmail.com>
2025-02-12 13:59:31 -0300

    docs: add fonts and assets

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter'
+import { useEffect } from "react";
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Hello World!!</Text>
