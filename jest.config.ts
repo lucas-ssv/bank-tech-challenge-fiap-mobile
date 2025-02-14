bf5e06153eae21ee32600091d20b5570f621fcc3
Lucas Vieira <lucas_99silva@hotmail.com>
2025-02-14 14:56:39 -0300

    chore: add jest and testing-library

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import { Config } from 'jest'
+
+const config: Config = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|@gluestack-ui/*|@gluestack-style/*|@legendapp/*|react-native-svg)'
+  ],
+  transform: {
+    '.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: true,
+        babelConfig: 'babel.config.js'
+      }
+    ]
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': '<rootDir>/tests/mocks/style-mock.ts'
+  }
+}
+
+export default config
