5c219fea54d6e87493878a36a5d1aa3a2a60aa81
Lucas Vieira <lucas_99silva@hotmail.com>
2025-02-27 16:05:37 -0300

    style: remove SafeAreaView from Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,19 +1,16 @@
 import { FinancialFlowChart, NewTransaction, Welcome } from './components'
 import { VStack } from '@/components/ui'
 import { Extract } from '@/components'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView } from 'react-native'
 
 export function Dashboard() {
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      <VStack className="flex-1 bg-custom-my-light-green px-6">
-        <SafeAreaView>
-          <Welcome />
-          <NewTransaction className="mt-6" />
-          <FinancialFlowChart className="mt-6" />
-          <Extract className="mt-6" />
-        </SafeAreaView>
+      <VStack className="flex-1 bg-custom-my-light-green p-6">
+        <Welcome />
+        <NewTransaction className="mt-6" />
+        <FinancialFlowChart className="mt-6" />
+        <Extract className="mt-6" />
       </VStack>
     </ScrollView>
   )
