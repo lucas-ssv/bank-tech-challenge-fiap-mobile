326da050d48d2963259c4ec895784cd5d0261cc7
Lucas Vieira <lucas_99silva@hotmail.com>
2025-02-27 18:38:37 -0300

    feat: add Dashboard screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,12 +1,15 @@
 import { Dashboard } from '@/screens'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { AvatarMenu } from '@/components'
+import { Drawer } from './components'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 
 const { Navigator, Screen } = createDrawerNavigator()
 
 export function AppRoutes() {
   return (
     <Navigator
+      drawerContent={(props) => <Drawer {...props} />}
       screenOptions={{
         headerTitle: '',
         headerStyle: {
@@ -22,9 +25,31 @@ export function AppRoutes() {
         headerRight() {
           return <AvatarMenu />
         },
+        drawerItemStyle: {
+          borderRadius: 8,
+        },
+        drawerActiveBackgroundColor: '#FFFFFF',
+        drawerActiveTintColor: '#004D61',
+        drawerStyle: {
+          backgroundColor: '#E4EDE3',
+        },
       }}
     >
-      <Screen name="Dashboard" component={Dashboard} />
+      <Screen
+        name="Início"
+        component={Dashboard}
+        options={{
+          drawerIcon(props) {
+            return (
+              <MaterialIcons
+                name="account-balance"
+                size={props.size}
+                color={props.color}
+              />
+            )
+          },
+        }}
+      />
     </Navigator>
   )
 }
