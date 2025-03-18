587190a1afa50b4cf107f86209532ad9a2b749ca
Lucas Vieira <lucas_99silva@hotmail.com>
2025-03-18 18:27:24 -0300

    feat: add blurred balance on press eye icon

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,16 +1,32 @@
-import { ComponentProps } from 'react'
-import { TouchableOpacity } from 'react-native'
+import { ComponentProps, useEffect, useState } from 'react'
+import { TouchableOpacity, View } from 'react-native'
 import { Box, Divider, Heading, HStack, Text } from '@/components/ui'
-import EyeIcon from '@/assets/olho.svg'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import Pixels from '@/assets/pixels.svg'
 import Illustration from '@/assets/ilustracao.svg'
 import { useAuth } from '@/contexts'
 import { getFormattedDate } from '@/utils'
+import { BlurView } from 'expo-blur'
+import { getStorageBalanceBlurred, setStorageBalanceBlurred } from '@/storage'
 
 type Props = ComponentProps<typeof Box>
 
 export function Welcome({ ...rest }: Props) {
   const { user } = useAuth()
+  const [balanceBlurred, setBalanceBlurred] = useState(true)
+
+  const handleBlurredBalance = async () => {
+    await setStorageBalanceBlurred(String(!balanceBlurred))
+    setBalanceBlurred(!balanceBlurred)
+  }
+
+  useEffect(() => {
+    async function loadBalanceBlurred() {
+      const balanceBlurred = await getStorageBalanceBlurred()
+      setBalanceBlurred(Boolean(balanceBlurred))
+    }
+    loadBalanceBlurred()
+  }, [])
 
   return (
     <Box
@@ -47,13 +63,34 @@ export function Welcome({ ...rest }: Props) {
       </Text>
       <HStack className="items-center gap-6 mt-10">
         <Heading className="text-white font-semibold text-lg">Saldo</Heading>
-        <TouchableOpacity>
-          <EyeIcon width={20} height={20} />
+        <TouchableOpacity onPress={handleBlurredBalance}>
+          {balanceBlurred ? (
+            <MaterialIcons name="visibility" size={20} color="#FFFFFF" />
+          ) : (
+            <MaterialIcons name="visibility-off" size={20} color="#FFFFFF" />
+          )}
         </TouchableOpacity>
       </HStack>
       <Divider className="h-[2px] my-4" />
       <Text className="text-white text-md">Conta Corrente</Text>
-      <Text className="text-white text-2xl font-body mt-2">R$ 2.500,00</Text>
+      <View>
+        {balanceBlurred && (
+          <BlurView
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              borderRadius: 8,
+              zIndex: 1,
+            }}
+            intensity={50}
+            tint="light"
+          />
+        )}
+        <Text className="text-white text-2xl font-body mt-2">R$ 2.500,00</Text>
+      </View>
     </Box>
   )
 }
